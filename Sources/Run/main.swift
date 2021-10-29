e867617397e33f1bd5deb3dcbd3509b34b2ef409
Alexandr Shipin <ssanek212@gmail.com>
2021-10-29 16:20:01 +0300

    Подзакрылся https

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -4,9 +4,6 @@ import Vapor
 var env = try Environment.detect()
 try LoggingSystem.bootstrap(from: &env)
 let app = Application(env)
-#if canImport(FoundationNetworking)
-    app.http.server.configuration.hostname = "176.57.214.20"
-#endif
 defer { app.shutdown() }
 try configure(app)
 try app.run()
---
+++ proxy/index.js
@@ -0,0 +1,15 @@
+	var httpProxy = require('http-proxy');
+	var fs = require('fs');
+	//
+	// Create the HTTPS proxy server in front of a HTTP server
+	//
+	httpProxy.createServer({
+		target: {
+			host: '127.0.0.1',
+			port: 8080
+		},
+		ssl: {
+			key: fs.readFileSync('key/key.pem', 'utf8'),
+			cert: fs.readFileSync('key/certificate.pem', 'utf8')
+		}
+	}).listen(8443);
